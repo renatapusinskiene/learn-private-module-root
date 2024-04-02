f2e53fc1976b6a3af0d193b49eb1fedce7b14f43
renatapusinskiene <45994137+renatapusinskiene@users.noreply.github.com>
2024-04-02 21:30:21 +0200

    Update main.tf update SOURCE as in TFC usage

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/testrenataorg/s3-webapp/aws"
+  source  = "app.terraform.io/testrenataorg/module-aws-s3-webapp/private"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
